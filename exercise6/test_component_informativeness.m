8fe1e9f333e7a52ef9627bd49392aad3c84dc50f
AndrejHafner <andrej.hafn3r@gmail.com>
2019-01-09 01:40:15 +0100

    Removed unnecessary plots

--- exercise6/test_component_informativeness.m
+++ exercise6/test_component_informativeness.m
@@ -4,11 +4,8 @@ A = preprocess_image_series(2);
 
 A_avg = mean(A,2);
 
-%imagesc(reshape(A_avg,[96,84]));
 [U,mean_val] = dual_pca(A);
-subplot(1,2,1);
-colormap gray;
-imagesc(A_avg);
+
 scale = 30;
 for x = linspace(-200,200,1200)
     A_proj = U' * (A_avg - mean_val);
